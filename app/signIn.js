d17262adbc8713006824cbb9a1fa0178a6cb7286
MohamadKalakesh <106426856+MohamadKalakesh@users.noreply.github.com>
2024-07-15 06:50:53 +0300

    Adding lottie-react-native + Implementing SignIn and SignUp UI

--- app/signIn.js
+++ app/signIn.js
@@ -1,15 +1,41 @@
-import { View, Text, Image, TextInput, TouchableOpacity } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  Image,
+  TextInput,
+  TouchableOpacity,
+  Pressable,
+  Alert,
+} from "react-native";
+import React, { useRef, useState } from "react";
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
 import { StatusBar } from "expo-status-bar";
 import { Octicons } from "@expo/vector-icons";
+import { useRouter } from "expo-router";
+import Loading from "../components/Loading";
+import CustomKeyboardView from "../components/CustomKeyboardView";
 
 export default function SignIn() {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+
+  const emailRef = useRef("");
+  const passwordRef = useRef("");
+
+  const handleLogin = async () => {
+    if (!emailRef.current || !passwordRef.current) {
+      Alert.alert("Sign In", "Missing Fields!");
+      return;
+    }
+
+    //login process
+  };
+
   return (
-    <View className="flex-1">
+    <CustomKeyboardView>
       <StatusBar style="dark" />
       <View
         style={{ paddingTop: hp(8), paddingHorizontal: wp(5) }}
@@ -38,6 +64,7 @@ export default function SignIn() {
             >
               <Octicons name="mail" size={hp(2.7)} color="gray" />
               <TextInput
+                onChangeText={(value) => (emailRef.current = value)}
                 style={{ fontSize: hp(2) }}
                 className="flex-1 font-semibold text-neutral-700"
                 placeholder="Email Address"
@@ -52,9 +79,11 @@ export default function SignIn() {
               >
                 <Octicons name="lock" size={hp(2.7)} color="gray" />
                 <TextInput
+                  onChangeText={(value) => (passwordRef.current = value)}
                   style={{ fontSize: hp(2) }}
                   className="flex-1 font-semibold text-neutral-700"
                   placeholder="Password"
+                  secureTextEntry
                   placeholderTextColor={"gray"}
                 />
               </View>
@@ -66,20 +95,47 @@ export default function SignIn() {
               </Text>
             </View>
 
-            <TouchableOpacity
-              style={{ height: hp(6.5) }}
-              className="bg-indigo-500 rounded-xl justify-center items-center"
-            >
+            <View>
+              {loading ? (
+                <View className="flex-row justify-center">
+                  <Loading size={hp(6.5)}></Loading>
+                </View>
+              ) : (
+                <TouchableOpacity onPress={handleLogin}>
+                  <View
+                    style={{ height: hp(6.5) }}
+                    className="bg-indigo-500 rounded-xl justify-center items-center"
+                  >
+                    <Text
+                      style={{ fontSize: hp(2.7) }}
+                      className="text-white font-bold tracking-wider"
+                    >
+                      Sign In
+                    </Text>
+                  </View>
+                </TouchableOpacity>
+              )}
+            </View>
+
+            <View className="flex-row justify-center">
               <Text
-                style={{ fontSize: hp(2.7) }}
-                className="text-black font-bold tracking-wider"
+                style={{ fontSize: hp(1.8) }}
+                className="font-semibold text-neutral-500"
               >
-                Sign In
+                Don't have an account?{" "}
               </Text>
-            </TouchableOpacity>
+              <Pressable onPress={() => router.push("signUp")}>
+                <Text
+                  style={{ fontSize: hp(1.8) }}
+                  className="font-semibold text-indigo-500"
+                >
+                  Sign Up
+                </Text>
+              </Pressable>
+            </View>
           </View>
         </View>
       </View>
-    </View>
+    </CustomKeyboardView>
   );
 }
