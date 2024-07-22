ca52f2f852d1c077bae5d62a408f031863503b76
MohamadKalakesh <106426856+MohamadKalakesh@users.noreply.github.com>
2024-07-22 06:32:01 +0300

    Implementing ChatList

--- app/(app)/home.js
+++ app/(app)/home.js
@@ -1,22 +1,51 @@
 import { View, Text, Pressable } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useAuth } from "../../context/authContext";
+import { StatusBar } from "expo-status-bar";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
+import Loading from "../../components/Loading";
+import ChatList from "../../components/ChatList";
+import { getDocs, query, where } from "firebase/firestore";
+import { usersRef } from "../../firebaseConfig";
 
 export default function Home() {
   const { logout, user } = useAuth();
 
-  const handleLogout = async () => {
-    await logout();
-  };
+  const [users, setUsers] = useState([]);
 
-  console.log(user);
+  useEffect(() => {
+    if (user?.uid) {
+      getUsers();
+    }
+  }, []);
+
+  const getUsers = async () => {
+    const q = query(usersRef, where("userId", "!=", user?.uid));
+    const querySnapshot = await getDocs(q);
+
+    let data = [];
+    querySnapshot.forEach((doc) => {
+      data.push({ ...doc.data() });
+    });
+
+    setUsers(data);
+  };
 
   return (
     <View className="flex-1 bg-white">
-      <Text>Hello {user.username}</Text>
-      <Pressable onPress={handleLogout}>
-        <Text>Sign Out</Text>
-      </Pressable>
+      <StatusBar style="light" />
+
+      {users.length > 0 ? (
+        <ChatList users={users} />
+      ) : (
+        <View className="flex items-center" style={{ top: hp(30) }}>
+          {/* <ActivityIndicator size="large" /> */}
+          <Loading size={hp(20)} />
+        </View>
+      )}
     </View>
   );
 }
